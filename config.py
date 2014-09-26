6888d08360c04d7bade2939dbba833b5002c38d8
joncl <jonclark1@gmail.com>
2014-09-26 13:03:31 -0700

    Update config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# IP or hostname of your MythTV backend
+hostname = "<ip or hostname>"
+
+# Port number of your MythTV backend (this is the default, can probably leave as-is)
+host_port = "6544"
+
+# A list of MythTV recording directories, full paths in quotes, separated by commas
+mythtv_recording_dirs = ["/pool/mythtv/recordings-kids/"]
+
+# Path to write symlinks
+symlinks_dir = "/pool/myth2xbmc/recordings/"
+
+# API key for TheTVDB
+ttvdb_key = "<your ttvdb key>"
+
+# Path to store TheTVDB series zips
+ttvdb_zips_dir = "/pool/myth2xbmc/ttvdb/"
+
+# API key for TheMovieDB
+tmdb_key = "<your tmdb key>"
